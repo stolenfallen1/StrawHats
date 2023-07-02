e7f21e8964468c76d2d4e2847133f79ea52b8ea0
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-07-03 03:55:52 +0800

    minor changes

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
+import Logo from "../Asset/HomeAssets/LuffyHat.png";
 
 const Navbar = () => {
   return (
-    <div>
-      <ul className="flex px-10 py-8 justify-end items-center text-xl font-medium tracking-tight bg-custom-red">
+    <div className="flex px-10 py-3 justify-between items-center bg-custom-red">
+      <div>
+        <img src={Logo} style={{ width: "85px", height: "auto" }} />
+      </div>
+      <ul className="flex items-center text-xl font-medium tracking-tight">
         <li className="px-3 text-custom-gold hover:text-opacity-70">
           <Link to="/">Home</Link>
         </li>
