8c54e7537c835d770acb446b3defbce304e4b30e
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-07-04 00:23:49 +0800

    Home bg image and brightness filter adjustment

--- src/Views/Home.tsx
+++ src/Views/Home.tsx
@@ -1,10 +1,26 @@
 import Navbar from "../Components/Navbar";
+import HomeContent from "../Components/HomeContent";
+import HomeWallpaper from "../Asset/HomeAssets/HomeWallpaper.jpeg";
 
 const Home = () => {
   return (
-    <div>
-      <Navbar />
-      <h1>Hello World</h1>
+    <div className="flex flex-col h-screen">
+      <div style={{ height: "12vh" }}>
+        <Navbar />
+      </div>
+      <div className="relative">
+        <img
+          src={HomeWallpaper}
+          className="absolute inset-0 w-full object-cover"
+          style={{ height: "88vh" }}
+        />
+        <div
+          className="absolute inset-0 bg-gradient-to-t from-black/70 to-black/30 z-999"
+          style={{ height: "88vh" }}
+        >
+          <HomeContent />
+        </div>
+      </div>
     </div>
   );
 };
