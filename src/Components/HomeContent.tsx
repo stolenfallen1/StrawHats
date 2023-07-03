22735bd1af6b25516669c602fece1b05d01d5531
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-07-04 01:19:02 +0800

    home content btns and its styling

--- src/Components/HomeContent.tsx
+++ src/Components/HomeContent.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const HomeContent = () => {
   return (
     <div className="flex justify-end px-5 py-40">
@@ -7,6 +9,11 @@ const HomeContent = () => {
             Unveiling Legends, Unleashing Dreams: Dive into the World of One
             Piece Characters.
           </h1>
+          <button className="mt-5 px-3 py-2 bg-black text-white border-2 border-custom-red hover:border-custom-gold ease-in duration-300">
+            <Link to="/characters" className="text-2xl">
+              See More
+            </Link>
+          </button>
         </div>
       </div>
       <div className="flex-1">
@@ -16,6 +23,11 @@ const HomeContent = () => {
               Embrace the Grand Line of Knowledge: Uniting One Piece Fans
               Worldwide.
             </h1>
+            <button className="mt-5 px-3 py-2 bg-black text-white border-2 border-custom-red hover:border-custom-gold ease-in duration-300">
+              <Link to="/forum" className="text-2xl">
+                Learn More
+              </Link>
+            </button>
           </div>
         </div>
       </div>
