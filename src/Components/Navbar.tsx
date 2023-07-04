811a1be4246fe7035a045e081a0ed9dd6db3b834
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-07-04 16:41:48 +0800

    redesign menu to just hamburger button

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,23 +1,18 @@
-import { Link } from "react-router-dom";
 import Logo from "../Asset/HomeAssets/LuffyHat.png";
+import { Squash as Hamburger } from "hamburger-react";
 
-const Navbar = () => {
+type NavbarProps = {
+  isOpen: boolean;
+  toggleMenu: () => void;
+};
+
+const Navbar = ({ isOpen, toggleMenu }: NavbarProps) => {
   return (
     <div className="flex px-10 py-3 justify-between items-center bg-custom-red">
       <div>
-        <img src={Logo} style={{ width: "85px", height: "auto" }} />
+        <img src={Logo} style={{ width: "85px", height: "auto" }} alt="Logo" />
       </div>
-      <ul className="flex items-center text-xl font-medium tracking-tight">
-        <li className="px-3 text-custom-gold hover:text-opacity-70">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="px-3 text-custom-gold hover:text-opacity-70">
-          <Link to="/characters">Characters</Link>
-        </li>
-        <li className="px-3 text-custom-gold hover:text-opacity-70">
-          <Link to="/forum">Forum</Link>
-        </li>
-      </ul>
+      <Hamburger toggled={isOpen} toggle={toggleMenu} />
     </div>
   );
 };
--- src/Views/Home.tsx
+++ src/Views/Home.tsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import Navbar from "../Components/Navbar";
 import HomeContent from "../Components/HomeContent";
 import HomeWallpaper from "../Asset/HomeAssets/HomeWallpaper.jpeg";
 
 const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="flex flex-col h-screen">
       <div style={{ height: "12vh" }}>
-        <Navbar />
+        <Navbar isOpen={isOpen} toggleMenu={toggleMenu} />
       </div>
       <div className="relative">
         <img
@@ -14,6 +22,21 @@ const Home = () => {
           className="absolute inset-0 w-full object-cover"
           style={{ height: "88vh" }}
         />
+        {isOpen && (
+          <div className="fixed top-0 left-0 w-full h-full bg-white">
+            <div className="flex flex-col items-center justify-center h-full text-xl font-medium tracking-tight">
+              <Link to="/" onClick={toggleMenu}>
+                Home
+              </Link>
+              <Link to="/characters" onClick={toggleMenu}>
+                Characters
+              </Link>
+              <Link to="/forum" onClick={toggleMenu}>
+                Forum
+              </Link>
+            </div>
+          </div>
+        )}
         <div
           className="absolute inset-0 bg-gradient-to-t from-black/70 to-black/50 z-999"
           style={{ height: "88vh" }}
