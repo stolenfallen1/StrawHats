d21335e9df1599feea033fcf2e3c4dcfe5e0b9c6
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-07-04 18:46:41 +0800

    adding basic animation

--- src/Views/Home.tsx
+++ src/Views/Home.tsx
@@ -23,31 +23,31 @@ const Home = () => {
           style={{ height: "88vh" }}
           alt="Home Wallpaper"
         />
-        {isOpen && (
-          <div
-            className="fixed inset-0 bg-black z-50"
-            style={{ backgroundColor: "rgba(0, 0, 0, 0.85)" }}
-            onClick={toggleMenu}
-          >
-            <div className="flex flex-col justify-center h-full text-xl tracking-tight">
-              <button className="text-white font-bold text-4xl py-2 hover:opacity-80">
-                <Link to="/" onClick={toggleMenu}>
-                  Home
-                </Link>
-              </button>
-              <button className="text-white font-bold text-4xl py-2 hover:opacity-80">
-                <Link to="/characters" onClick={toggleMenu}>
-                  Characters
-                </Link>
-              </button>
-              <button className="text-white font-bold text-4xl py-2 hover:opacity-80">
-                <Link to="/forum" onClick={toggleMenu}>
-                  Forum
-                </Link>
-              </button>
-            </div>
+        <div
+          className={`fixed inset-0 top-0 left-0 bg-black z-50 transition-transform ${
+            isOpen ? "translate-y-0" : "translate-y-full"
+          }`}
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.85)" }}
+          onClick={toggleMenu}
+        >
+          <div className="flex flex-col justify-center h-full text-xl tracking-tight">
+            <button className="text-white font-bold text-4xl py-2 hover:opacity-80">
+              <Link to="/" onClick={toggleMenu}>
+                Home
+              </Link>
+            </button>
+            <button className="text-white font-bold text-4xl py-2 hover:opacity-80">
+              <Link to="/characters" onClick={toggleMenu}>
+                Characters
+              </Link>
+            </button>
+            <button className="text-white font-bold text-4xl py-2 hover:opacity-80">
+              <Link to="/forum" onClick={toggleMenu}>
+                Forum
+              </Link>
+            </button>
           </div>
-        )}
+        </div>
         <div
           className="absolute inset-0 bg-gradient-to-t from-black/70 to-black/50 z-999"
           style={{ height: "88vh" }}
