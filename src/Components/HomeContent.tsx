4909e501e59911a63b270471021a3e179fb9a593
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-07-06 01:13:43 +0800

    added a functionality that conditionally renders tsx elements base on a specific screen size

--- src/Components/HomeContent.tsx
+++ src/Components/HomeContent.tsx
@@ -1,28 +1,37 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const HomeContent = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 640);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="flex justify-end px-5 py-40">
-      <div className="flex-1">
-        <div className="w-[400px] mx-auto">
-          <h1 className="text-4xl text-custom-gold font-bold leading-normal">
-            Unveiling Legends, Unleashing Dreams: Dive into the World of One
-            Piece Characters.
-          </h1>
-          <button className="mt-5 px-3 py-2 bg-black text-white border-2 border-custom-red hover:border-custom-gold ease-in duration-300">
-            <Link to="/characters" className="text-2xl">
-              See More
-            </Link>
-          </button>
-        </div>
-      </div>
-      <div className="flex-1">
-        <div className="text-right">
-          <div className="w-[400px] mx-auto text-right">
+      {isSmallScreen ? (
+        <div className="flex-1">
+          <div className="w-[400px] mx-auto">
             <h1 className="text-4xl text-custom-gold font-bold leading-normal">
-              Embrace the Grand Line of Knowledge: Uniting One Piece Fans
+              Embark on a Legendary Voyage: Uniting One Piece Enthusiasts
               Worldwide.
             </h1>
+            <button className="mt-5 mr-5 px-3 py-2 bg-black text-white border-2 border-custom-red hover:border-custom-gold ease-in duration-300">
+              <Link to="/characters" className="text-2xl">
+                See More
+              </Link>
+            </button>
             <button className="mt-5 px-3 py-2 bg-black text-white border-2 border-custom-red hover:border-custom-gold ease-in duration-300">
               <Link to="/forum" className="text-2xl">
                 Learn More
@@ -30,7 +39,38 @@ const HomeContent = () => {
             </button>
           </div>
         </div>
-      </div>
+      ) : (
+        <>
+          <div className="flex-1">
+            <div className="w-[400px] mx-auto">
+              <h1 className="text-4xl text-custom-gold font-bold leading-normal">
+                Unveiling Legends, Unleashing Dreams: Dive into the World of One
+                Piece Characters.
+              </h1>
+              <button className="mt-5 px-3 py-2 bg-black text-white border-2 border-custom-red hover:border-custom-gold ease-in duration-300">
+                <Link to="/characters" className="text-2xl">
+                  See More
+                </Link>
+              </button>
+            </div>
+          </div>
+          <div className="flex-1">
+            <div className="text-right">
+              <div className="w-[400px] mx-auto text-right">
+                <h1 className="text-4xl text-custom-gold font-bold leading-normal">
+                  Embrace the Grand Line of Knowledge: Uniting One Piece Fans
+                  Worldwide.
+                </h1>
+                <button className="mt-5 px-3 py-2 bg-black text-white border-2 border-custom-red hover:border-custom-gold ease-in duration-300">
+                  <Link to="/forum" className="text-2xl">
+                    Learn More
+                  </Link>
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
