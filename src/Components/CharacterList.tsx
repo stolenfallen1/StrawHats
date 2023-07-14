369cf2e2616b88eac9ede992c2d6eedfe6076d59
stolenfallen1 <stolenfallen1@gmail.com>
2023-07-14 23:55:19 +0800

    create seperate component for character lists

---
+++ src/Components/CharacterList.tsx
@@ -0,0 +1,15 @@
+import luffy from "../Asset/CharacterAssets/luffy.png";
+import zoro from "../Asset/CharacterAssets/zoro.png";
+
+const CharacterList = () => {
+  return (
+    <div>
+      <div>
+        <img src={luffy} />
+        <img src={zoro} />
+      </div>
+    </div>
+  );
+};
+
+export default CharacterList;
--- src/Views/CharacterPage.tsx
+++ src/Views/CharacterPage.tsx
@@ -1,20 +1,5 @@
-import { useState } from "react";
-import Navbar from "../Components/Navbar";
-
 const CharacterPage = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  }
-
-  return (
-    <div>
-      <div>
-        <Navbar isOpen={isOpen} toggleMenu={toggleMenu} />
-      </div>
-    </div>
-  );
+  return <div className="h-screen"></div>;
 };
 
 export default CharacterPage;
