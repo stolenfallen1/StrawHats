a447e27f8a1e545086e3a84b7a5d8967a500e03b
stolenfallen1 <stolenfallen1@gmail.com>
2023-07-21 00:01:45 +0800

    minor revisions

--- src/Components/ForumCards.tsx
+++ src/Components/ForumCards.tsx
@@ -6,7 +6,10 @@ interface ForumCardsProps {
 
 const ForumCards = ({ header, body, comments }: ForumCardsProps) => {
   return (
-    <div className="border-2 border-gray-300 px-6 py-3">
+    <div
+      className="m-10 flex flex-col items-center justify-center border-2 border-gray-300 px-6 py-3 rounded-lg"
+      style={{ width: "280px", height: "280px" }}
+    >
       <h1 className="text-custom-gold text-xl font-extrabold mb-3">{header}</h1>
       <p className="text-lg mb-4">{body}</p>
       <p className="text-md">{comments}</p>
--- src/Components/ForumCardsContainer.tsx
+++ src/Components/ForumCardsContainer.tsx
@@ -1,9 +1,24 @@
 import ForumCards from "./ForumCards";
 
+const cardData = [
+  { header: "Header 1", body: "Body 1", comments: "Comments 1" },
+  { header: "Header 2", body: "Body 2", comments: "Comments 2" },
+  { header: "Header 3", body: "Body 3", comments: "Comments 3" },
+];
+
 const ForumCardsContainer = () => {
   return (
-    <div style={{ display: "flex" }}>
-      <ForumCards />
+    <div className="pt-5">
+      <div className="flex justify-center items-center">
+        {cardData.map((card, index) => (
+          <ForumCards
+            key={index}
+            header={card.header}
+            body={card.body}
+            comments={card.comments}
+          />
+        ))}
+      </div>
     </div>
   );
 };
